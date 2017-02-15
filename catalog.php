4f1537c3e89119070db1402fbc5a34f85539c383
Creative Roderick <CreativeRoderick@gmail.com>
2017-02-15 11:43:55 -0600

    Updated
    Added Catalog List, No Formatting was done

--- catalog.php
+++ catalog.php
@@ -5,6 +5,12 @@
 -->
 
 <?php
+$catalog = array(
+  "Design Patterns",
+  "Forrest Gump",
+  "Beethoven"
+);
+
 $pageTitle = "Full Catalog";
 $section = null;
 
@@ -23,7 +29,22 @@ if(isset($_GET["cat"])) {
 }
 
 include("inc/header.php"); ?>
-  <div class="section page">
-    <h1><?php echo $pageTitle; ?></h1>
+  <div class="section catalog page">
+
+    <div class="wrapper">
+
+      <h1><?php echo $pageTitle; ?></h1>
+      <ul>
+        <!--Added a foreach here to display or array items we list-->
+        <?php
+          foreach($catalog as $item) {
+            echo "<li>" . $item . "</li>";
+        }
+        ?>
+      </ul>
+
+    </div>
+
   </div>
+
 <?php include("inc/footer.php"); ?>
