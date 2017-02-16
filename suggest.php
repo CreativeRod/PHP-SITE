85fc4e3c854d52a28d17eb8461c4b2f9f01f7c9c
Creative Roderick <CreativeRoderick@gmail.com>
2017-02-16 16:12:48 -0600

    Form Work
    Wrapped up the pages, not i'm working on the form

--- suggest.php
+++ suggest.php
@@ -1,12 +1,32 @@
-<!-- Always add the var for the page title before the include file -->
 <?php
-$pageTitle = "Suggest A Media Item";
+$pageTitle = "Suggest a Media Item";
 $section = "suggest";
 
 include("inc/header.php"); ?>
 
 <div class="section page">
-  <h1>Suggest A Media Item</h1>
+  <div class="wrapper">
+    <h1>Suggest a Media Item</h1>
+    <p>If You Think There Is Something I&rsquo;m Missing Let Me Know.</p>
+    <form method="post">
+      <table>
+        <tr>
+          <th><label for="name">Name</label></th>
+          <td><input type="text" name="name" id="name"/></td>
+        </tr>
+        <tr>
+          <th><label for="email">Email</label></th>
+          <td><input type="text" name="email" id="email"/></td>
+        </tr>
+        <tr>
+          <th><label for="details">Suggest...</label></th>
+          <td><textarea name="details" id="details"></textarea></td>
+        </tr>
+      </table>
+      <input type="submit" value="Send"/>
+    </form>
+  </div>
+
 </div>
 
 <?php include("inc/footer.php"); ?>
